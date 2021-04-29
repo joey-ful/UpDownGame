e485f1cb38af8efc917f14f099d287330d0f270a
jehong <jehong@student.42seoul.com>
2021-04-29 16:40:55 +0900

    UpDownGame done

---
+++ UpDownGame/CreditViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CreditViewController.swift
+//  UpDownGame
+//
+//  Created by JeongAh Hong on 2021/04/29.
+//
+
+import UIKit
+
+class CreditViewController: UIViewController {
+
+    @IBAction func touchUpDismissButton(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
